dc05b437381c5080c0b40c563876d869e234dc02
William Hideki Nakata <whnakata@gmail.com>
2023-03-21 07:22:17 -0300

    inserting loss log history

--- NN/NeuralNetwork.py
+++ NN/NeuralNetwork.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from NN.utils import accuracy
+
 
 class SequentialNeuralNet:
     def __init__(self, layers: list):
@@ -14,7 +16,7 @@ class SequentialNeuralNet:
         for li in range(1, len(self.layers)):
             self.layers[li].build(self.layers[li - 1].neurons)
 
-    def fit(self, x, y, epochs=10, lr=1e-3):
+    def fit(self, x, y, epochs=10, lr=1e-3, showAccuracy=False):
         if len(y.shape) == 1:
             y = y.reshape((len(y), 1))
         self.loss = []
@@ -26,12 +28,14 @@ class SequentialNeuralNet:
                 error = self.layers[l].apply_grad(error, lr)
                 # print(error)
 
-            # forw = self.predict(x)
-            # self.loss.append(self.loss_fn.loss(y, forw).mean())
-            # if e % (epochs // 100 + 1) == 0:
-            #     back = '\b'
-            #     status = f'Epoch {e}/{epochs} - loss: {self.loss_fn.dLoss(y, forw).mean():.4e}'
-            #     print(f'{back * len(status)}{status}', end='', flush=True)
+            forw = self.predict(x)
+            self.loss.append(self.loss_fn.loss(y, forw).mean())
+            if e % (epochs // 100 + 1) == 0:
+                back = '\b'
+                status = f'Epoch {e}/{epochs} - loss: {self.loss_fn.loss(y, forw).mean():.4e}'
+                if showAccuracy:
+                    status += f" Accuracy: {accuracy(y, forw):.2f}"
+                print(f'{back * len(status)}{status}', end='', flush=True)
 
     def predict(self, x):
         self.layers[0].a = x
